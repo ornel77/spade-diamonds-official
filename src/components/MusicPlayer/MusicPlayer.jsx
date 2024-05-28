7ee2d6e5aedb03233a8094270f5428d2b7a6fb04
ornel77 <ornella.tomboza@gmail.com>
2024-05-28 16:48:12 +0200

    start bg of music player

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import Card from './Card'
-import './MusicPlayer.scss'
+import { useState } from "react";
+import Card from "./Card";
+import "./MusicPlayer.scss";
 
 const MusicPlayer = () => {
-  const [musicNumber, setMusicNumber] = useState(2)
+  const [musicNumber, setMusicNumber] = useState(0);
   return (
-    <div className="container-player ">
-      <div className="music-container min-h-screen">
+    <div className="container-player min-h-screen">
+      <div className="sub-container">
+        <div className="shape shape1"></div>
+        <div className="shape shape2"></div>
+        <div className="shape shape3"></div>
         {/* <h2>Check Out Our Musics</h2> */}
-      <main className=' rounded-lg'>
-        <Card props={{ musicNumber, setMusicNumber }} />
-      </main>
+
+        <main className=" rounded-lg">
+          <Card props={{ musicNumber, setMusicNumber }} />
+        </main>
       </div>
     </div>
-  )
-}
-export default MusicPlayer
+  );
+};
+export default MusicPlayer;
