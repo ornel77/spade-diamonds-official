fd8aa82bef4f01f3b903b02f2c5fa1cb49b18c7e
ornel77 <ornella.tomboza@gmail.com>
2024-05-30 11:03:43 +0200

    background for music section ok

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -5,17 +5,13 @@ import "./MusicPlayer.scss";
 const MusicPlayer = () => {
   const [musicNumber, setMusicNumber] = useState(0);
   return (
-    <div className="container-player min-h-screen">
-      <div className="sub-container">
-        <div className="shape shape1"></div>
-        <div className="shape shape2"></div>
-        <div className="shape shape3"></div>
-        {/* <h2>Check Out Our Musics</h2> */}
-
-        <main className=" rounded-lg">
-          <Card props={{ musicNumber, setMusicNumber }} />
-        </main>
-      </div>
+    <div className="container-player rounded-lg w-[360px] relative">
+      {/* <div className="shape shape1"></div>
+      <div className="shape shape2"></div>
+      <div className="shape shape3"></div> */}
+      <main className=" rounded-lg">
+        <Card props={{ musicNumber, setMusicNumber }} />
+      </main>
     </div>
   );
 };
--- src/pages/MusicScreen.jsx
+++ src/pages/MusicScreen.jsx
@@ -2,11 +2,15 @@
 // import Musics from '../components/Musics/Musics'
 import MusicPlayer from '../components/MusicPlayer/MusicPlayer'
 
+import '../components/MusicPlayer/MusicPlayer.scss'
+
 const MusicScreen = () => {
   return (
-    <>
+    <div className='large-container min-h-screen flex justify-center items-center flex-col gap-7'>
+      <h2 className=' text-xl'>Check Out Our Musics</h2>
+      
       <MusicPlayer />
-    </>
+    </div>
   )
 }
 
