93167fe3fb38c5e9007d766a097cf8479732c282
ornel77 <ornella.tomboza@gmail.com>
2025-08-31 16:44:05 +0200

    updating live tour

--- src/components/ShowsNew/ShowItem.jsx
+++ src/components/ShowsNew/ShowItem.jsx
@@ -1,12 +1,12 @@
 const ShowItem = ({show}) => {
   return (
-    <article className="border-b py-5 grid grid-cols-1 gap-3 md:grid-cols-3 md:items-start">
+    <article className="border-b py-5 grid grid-cols-1 gap-2 md:grid-cols-3 md:items-start">
       <div className="flex gap-4">
         <p>{show.date}</p>
         <p>{show.time}</p>
       </div>
       <p className="">{show.venue}</p>
-      <p className="md:text-right">{show.city}</p>
+      <p className="md:text-right">{show.location}</p>
     </article>
   );
 };
--- src/components/ShowsNew/Shows.jsx
+++ src/components/ShowsNew/Shows.jsx
@@ -7,12 +7,18 @@ const Shows = () => {
     <section className="container">
       <div className="show-container">
         <h2>Live Tour</h2>
+        <p className="italic text-center text-lg mb-9 text-pink-500">No dates yet</p>
         <div>
-          {
-            shows.map(show => (
-              <ShowItem key={show.id} show={show} />
-            ))
-          }
+          <p className="text-center mb-5 underline">Past shows:</p>
+          <select name="show-year" id="show-year" className="bg-transparent w-full border-2 outline-none">
+            <option value="2025">2025</option>
+            <option value="2024">2024</option>
+            <option value="2024">2024</option>
+            <option value="2024">2024</option>
+          </select>
+          {shows.map((show) => (
+            <ShowItem key={show.id} show={show} />
+          ))}
         </div>
       </div>
     </section>
