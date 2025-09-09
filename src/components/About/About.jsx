2bd8749886a41ec0d3926f8f2231d41ffce1d6e2
ornel77 <ornella.tomboza@gmail.com>
2025-09-09 15:40:42 +0200

    add the real bio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,33 @@
-
-import illu from '../../assets/image/sd7.jpeg'
-import './About.scss'
+import illu from "../../assets/image/sd7.jpeg";
+import "./About.scss";
 
 function About() {
   return (
-    <section className='container'>
+    <section className="container">
       <div className="about-container">
-              <h2>About Us</h2>
+        <h2>About Us</h2>
         <div className="about-sub-container">
-            <div className='about-content'>
-              <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci itaque corrupti ex est perferendis possimus maxime rem necessitatibus minima consequatur pariatur, recusandae amet animi quasi velit atque quam assumenda quisquam?
-              </p>
-            </div>
-            <div className="about-illustration">
-              <img src={illu} alt="illustration" loading='lazy' />
-            </div>
+          <div className="about-content">
+            <p>
+              
+              Spade Diamonds, it&apos;s a family affair. A project started in 2008
+              with the initial sole purpose of playing “la fête de la musique”
+              in Meaux, where they are based. This lead to numerous musical
+              projects: The first musical contest win in 2009, the first EP in
+              2010, and a series of concerts throughout Paris and neighbouring
+              cities, including venues such as le File7, l&apos;espace Caravalle, le
+              Gibus, le Truskel and Les Disquaires.
+              <br /><br />
+                Spade Diamonds it&apos;s also a
+              sound. British-rock based, French pop infused and classical
+              musically coated, Spade Diamonds&apos; sound evolves with freedom
+              between several universe, and becomes whole on stage where it is
+              delivered with passion and intensity.
+            </p>
+          </div>
+          <div className="about-illustration">
+            <img src={illu} alt="illustration" loading="lazy" />
+          </div>
         </div>
       </div>
     </section>
