55bf56165c9388b632a9c503e1e67199c492f756
ornel77 <ornella.tomboza@gmail.com>
2026-04-28 23:11:43 +0200

    add mask to img and change position of function

--- src/components/Members/Members.jsx
+++ src/components/Members/Members.jsx
@@ -12,30 +12,31 @@ function Members() {
           <div
             className="sub-container"
           >
-            <img src="/sd8.webp" alt="wendy" loading="lazy" />
-            <p>
-              <span className="name">Wendy</span>
-              <span className="function"> - {t("members.function-w")}</span>
-            </p>
+            <img src="/sd8.webp" alt="wendy" loading="lazy" className="shadow-2xl"/>
+            <div>
+              <p className="name">Wendy</p>
+              <p className="function">{t("members.function-w")}</p>
+            </div>
           </div>
 
           <div
             className="sub-container"
           >
+        
             <img src="/sd9.jpg" alt="ed" loading="lazy" />
-            <p>
-              <span className="name">Edwin</span>
-              <span className="function"> - {t("members.function-e")}</span>
-            </p>
+            <div>
+              <p className="name">Edwin</p>
+              <p className="function">{t("members.function-e")}</p>
+            </div>
           </div>
           <div
             className="sub-container"
           >
             <img src="/sd10.webp" alt="ornel" loading="lazy" />
-            <p>
-              <span className="name">Ornella</span>
-              <span className="function"> - {t("members.function-o")}</span>
-            </p>
+            <div>
+              <p className="name">Ornella</p>
+              <p className="function">{t("members.function-o")}</p>
+            </div>
           </div>
         </div>
       </div>
